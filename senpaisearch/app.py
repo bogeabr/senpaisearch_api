ae9ca7e86204b982229eb0afdd7f73af35371929
bogeabr <bogea.matheus76@gmail.com>
2025-06-30 18:46:20 -0300

    Corrigindo blocos de imports

--- senpaisearch/app.py
+++ senpaisearch/app.py
@@ -6,6 +6,7 @@ from senpaisearch.routers import auth, characters, users
 from senpaisearch.schemas import Message
 
 app = FastAPI()
+
 app.include_router(users.router)
 app.include_router(auth.router)
 app.include_router(characters.router)
